4276a694d9322088061b900818aba0819eeb011d
luisaes02 <96957853+luisaes02@users.noreply.github.com>
2022-11-22 15:52:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
+       # tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
         tl_admin_user = 'Stvz20'
